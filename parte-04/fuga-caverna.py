01834a183537d78360c7106e1762bfdfb2a90eaf
Rafael Henrique Pinotti <rhpinotti@hotmail.com>
2021-01-25 16:34:37 -0300

    parte-4

---
+++ parte-04/fuga-caverna.py
@@ -0,0 +1,76 @@
+#Título da janela
+TITLE = 'Fuga da Caverna'
+#Largura da janela
+WIDTH = 800
+# Altura da janela
+HEIGHT = 679
+
+#Função para desenhar na tela
+def draw():
+    screen.blit('caverna', (0, 0))
+    morcego.draw()
+    estalactite_pequena.draw()
+    estalagmite_pequena.draw()
+
+#Aqui a gente pega o clique do mouse
+def on_mouse_down():
+    if (morcego.alive):
+        morcego.speed = -6.5
+
+#Função para reinicializar o jogo
+def reset():
+    morcego.speed = 1
+    morcego.center = (75, 350)
+    estalactite_pequena.center = (300,0)
+    estalagmite_pequena.center = (300, estalactite_pequena.height + espaco)
+    morcego.alive = True
+    morcego.image = 'morcego1'
+
+#Função de atualização. Normalmente é chamada 60 vezes por segundo
+def update():
+    morcego.speed += gravidade
+    morcego.y += morcego.speed
+    estalactite_pequena.x -= velocidade_tela
+    estalagmite_pequena.x -= velocidade_tela
+
+    #Verificando se a estalactite pequena saiu da tela (começo da tela, x = 0).
+    #Caso a posição x seja menor que o início da tela (x < 0), então desenha a estalactite e estalagmite no final da tela.
+    if estalactite_pequena.x < 0:
+        estalactite_pequena.x = WIDTH
+        estalagmite_pequena.x = WIDTH
+
+    #Verifica se o morcego atingiu a parte de baixo da tela (neste caso um pouco antes).
+    #Se atingiu, reseta o jogo.
+    if morcego.y > HEIGHT - 150:
+        reset()
+
+    #Verifica se o morcego bate na estalactite
+    if (morcego.colliderect(estalactite_pequena)):
+        colisao()
+
+#Função para tratar as colisões
+def colisao():
+    morcego.image = "morcego-fantasma"
+    morcego.alive = False
+
+#Adicionando o morcego
+morcego = Actor('morcego1', (75, 350))
+
+#Velocidade do morcego
+morcego.speed = 1
+
+#Definindo a velocidade da tela
+velocidade_tela = 1
+
+#Definindo o espaço entre estalactites e estalagmites, para o morcego passar no meio
+espaco = 200
+
+#Gravidade
+gravidade = 0.3
+
+#Adicionando os obstáculos
+estalactite_pequena = Actor('estalactite1', (300, 0))
+estalagmite_pequena = Actor('estalagmite1', (300, estalactite_pequena.height + espaco))
+
+#Configurando o jogo
+reset()
