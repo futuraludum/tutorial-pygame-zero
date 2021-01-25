bd32581a3c2e573952482eb4af10902247e45dc7
Rafael Henrique Pinotti <rhpinotti@hotmail.com>
2021-01-25 20:09:16 -0300

    parte-02

--- parte-02/caverna.py
+++ parte-02/caverna.py
@@ -4,3 +4,22 @@ TITLE = 'Voando na Caverna'
 WIDTH = 800
 # Altura da janela
 HEIGHT = 679
+
+#Esta função executa em torno de 60 vezes por segundo
+def update():
+    morcego.y += morcego.speed
+
+#Desenha os objetos na tela
+def draw():
+    screen.blit('caverna', (0, 0))
+    morcego.draw()
+
+#Executa sempre que o botão do mouse é pressionado
+def on_mouse_down():
+    morcego.y -= 50
+
+#Cria o personagem morcego do jogo
+morcego = Actor('morcego1', (75, 350))
+
+#Velocidade do morcego
+morcego.speed = 1
